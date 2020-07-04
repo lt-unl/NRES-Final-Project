affc9865ac78971d779712f370cdb132deae46d4
Michael Ou <ougengxin@gmail.com>
2020-07-04 11:51:32 -0500

    Update example_concat_zipcsv.py

--- example_concat_zipcsv.py
+++ example_concat_zipcsv.py
@@ -2,11 +2,40 @@
 import pandas as pd
 import zipfile
 
-dfs = []
-with zipfile.ZipFile('F:/Downloads/Compressed/Rulo_5SW.zip') as z:     #   z.infolist()
-    for d in pd.date_range('2019-08-24', '2019-08-30', freq='D'):
-        with z.open('Rulo_5SW/Rulo_5SW_soil_{:%Y%m%d}_0703.csv'.format(d)) as f:
-            dfs.append(pd.read_csv(f, header=0, skiprows=1).iloc[2:])
-        
+def openByDate(zipfpath, statedate, enddate):
+    '''
+    open zip and read files within the range of the starting date and ending date
+    preferable, controllable
+    '''
+    dfs = []
+    with zipfile.ZipFile(zipfpath) as z:     #   z.infolist()
+        filenames = [f.filename f in z.infolist()]
+        for d in pd.date_range(startdate, enddate, freq='D'):
+            toOpen = 'Rulo_5SW/Rulo_5SW_soil_{:%Y%m%d}_0703.csv'.format(d)
+            if toOpen in filenames:
+                # check if the file exists in the zip
+                with z.open(toOpen) as f:
+                    dfs.append(pd.read_csv(f, header=0, skiprows=1).iloc[2:])
+    return pd.concat(dfs).reset_index(drop=True)
 
-df_all = pd.concat(dfs).reset_index(drop=True)
+
+def openAllExisting(zipfpath):
+    '''
+    passively open all files. may open some files you don't want and make cleaning up more difficult
+    '''
+    
+    soil = []
+    agl = []
+    with zipfile.ZipFile(zipfpath) as z:     #   
+        for f in z.infolist():
+            fname = f.filename
+            if fname.endswith('csv'):
+                # check if it is a CSV file then open it
+                with z.open(fname) as f:
+                    df = pd.read_csv(f, header=0, skiprows=1).iloc[2:]
+                if 'soil' in fname:
+                    soil.append(df)
+                if 'agl' in fname:
+                    agl.append(df)
+
+    return pd.concat(agl).reset_index(drop=True), pd.concat(soil).reset_index(drop=True)
